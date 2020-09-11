cd9c379015feccaebdee1ccff8599ea790a1f67c
boyhoodbaby <boyhoodbaby@163.com>
2020-09-11 21:49:29 +0800

    对整个进行整体架构

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,9 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren':0,
+    'quote-props':0,
+    'vue/no-unused-vars':0
   }
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,54 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import MSite from '../views/MSite/MSite.vue'
+import Search from '../views/Search/Search.vue'
+import Profile from '../views/Profile/Profile.vue'
+import Order from '../views/Order/Order.vue'
+import Login from '../views/Login/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/msite',
+    name: 'MSite',
+    component: MSite,
+    meta: {
+      showFooter: true
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/search',
+    name: 'Search',
+    component: Search,
+    meta: {
+      showFooter: true
+    }
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile,
+    meta: {
+      showFooter: true
+    }
+  },
+  {
+    path: '/order',
+    name: 'Order',
+    component: Order,
+    meta: {
+      showFooter: true
+    }
+  },
+  {
+    path: '/',
+    redirect: 'msite'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
 
